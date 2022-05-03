cb35ae87473f7b7e2ad8fd12c0aa962061048d60
Tobias Jungmann <tobias.jungmann@tum.de>
2022-05-03 22:14:24 +0200

    added dialog to show icon sources

--- app/src/main/java/com/example/rpicommunicator_v1/ViewAndModels/MainActivity.kt
+++ app/src/main/java/com/example/rpicommunicator_v1/ViewAndModels/MainActivity.kt
@@ -1,150 +1,154 @@
-package com.example.rpicommunicator_v1.ViewAndModels;
+package com.example.rpicommunicator_v1.ViewAndModels
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.SeekBar;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
-import com.example.rpicommunicator_v1.Plants.PlantAdapter;
-import com.example.rpicommunicator_v1.R;
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private EditText destText;
-    private EditText startText;
-    private MainActivityViewModel mainActivityViewModel;
-    private CommunicationInterface communicationInterface;
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface.BOLD
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.rpicommunicator_v1.R
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
-        communicationInterface = new ViewModelProvider(this).get(CommunicationInterface.class);
-
-        initIO();
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var destText: EditText? = null
+    private var startText: EditText? = null
+    private var mainActivityViewModel: MainActivityViewModel? = null
+    private var communicationInterface: CommunicationInterface? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mainActivityViewModel = ViewModelProvider(this).get(
+            MainActivityViewModel::class.java
+        )
+        communicationInterface = ViewModelProvider(this).get(
+            CommunicationInterface::class.java
+        )
+        initIO()
     }
 
-
-    private void initIO() {
-        destText = findViewById(R.id.inputDest);
-        startText = findViewById(R.id.inputStart);
-
-        findViewById(R.id.button).setOnClickListener(this);
-
-        findViewById(R.id.time_button).setOnClickListener(this);
-        findViewById(R.id.button_weather).setOnClickListener(this);
-        findViewById(R.id.button_relais1).setOnClickListener(this);
-        findViewById(R.id.button_relais2).setOnClickListener(this);
-        findViewById(R.id.button_arduino1).setOnClickListener(this);
-        findViewById(R.id.button_arduino2).setOnClickListener(this);
-        findViewById(R.id.button_songtitle).setOnClickListener(this);
-        findViewById(R.id.button_outlet1).setOnClickListener(this);
-        findViewById(R.id.button_outlet2).setOnClickListener(this);
-        findViewById(R.id.button_outlet3).setOnClickListener(this);
-        findViewById(R.id.button_bikeActivity).setOnClickListener(this);
-        findViewById(R.id.button_settingsActivity).setOnClickListener(this);
-        findViewById(R.id.button_plantoverviewActivity).setOnClickListener(this);
-
-        final SeekBar sk = findViewById(R.id.seekBar);
-        sk.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            private int currentProgress = 0;
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                Log.d("seekbar", "onStopTrackingTouch");
-                currentProgress = ((int) (currentProgress * 2)) + 55;
-                Toast.makeText(getApplicationContext(), String.valueOf(currentProgress), Toast.LENGTH_LONG).show();
-                communicationInterface.sendText("newBrightness:" + currentProgress);
+    private fun initIO() {
+        destText = findViewById(R.id.inputDest)
+        startText = findViewById(R.id.inputStart)
+        findViewById<View>(R.id.button).setOnClickListener(this)
+        findViewById<View>(R.id.time_button).setOnClickListener(this)
+        findViewById<View>(R.id.button_weather).setOnClickListener(this)
+        findViewById<View>(R.id.button_relais1).setOnClickListener(this)
+        findViewById<View>(R.id.button_relais2).setOnClickListener(this)
+        findViewById<View>(R.id.button_arduino1).setOnClickListener(this)
+        findViewById<View>(R.id.button_arduino2).setOnClickListener(this)
+        findViewById<View>(R.id.button_songtitle).setOnClickListener(this)
+        findViewById<View>(R.id.button_outlet1).setOnClickListener(this)
+        findViewById<View>(R.id.button_outlet2).setOnClickListener(this)
+        findViewById<View>(R.id.button_outlet3).setOnClickListener(this)
+        findViewById<View>(R.id.button_bikeActivity).setOnClickListener(this)
+        findViewById<View>(R.id.button_settingsActivity).setOnClickListener(this)
+        findViewById<View>(R.id.button_plantoverviewActivity).setOnClickListener(this)
+        findViewById<View>(R.id.textViewSources).setOnClickListener(this)
+        val sk = findViewById<SeekBar>(R.id.seekBar)
+        sk.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            private var currentProgress = 0
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                Log.d("seekbar", "onStopTrackingTouch")
+                currentProgress = (currentProgress * 2) + 55
+                Toast.makeText(applicationContext, currentProgress.toString(), Toast.LENGTH_LONG)
+                    .show()
+                communicationInterface!!.sendText("newBrightness:$currentProgress")
             }
 
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                Log.d("seekbar", "onStartTrackingTouch");
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                Log.d("seekbar", "onStartTrackingTouch")
             }
 
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.d("seekbar", "onProgresschanged");
-                currentProgress = progress;
-
-
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                Log.d("seekbar", "onProgresschanged")
+                currentProgress = progress
             }
-        });
+        })
     }
 
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.button) {
-            Log.i("buttonClick", "Übernehmen was clicked");
-            communicationInterface.sendText("Stations;" + startText.getText().toString() + ";" + destText.getText().toString());
-
-        } else if (v.getId() == R.id.button_weather) {
-            Log.i("buttonClick", "weather button was clicked");
-            communicationInterface.sendText("weather");
-        } else if (v.getId() == R.id.time_button) {
-            Log.i("buttonClick", "time_button was clicked");
-            communicationInterface.sendText("changetime");
-        } else if (v.getId() == R.id.button_relais1) {
-            Log.i("buttonClick", "Relais 1 was clicked");
-            communicationInterface.sendText("relais1");
-        } else if (v.getId() == R.id.button_relais2) {
-            Log.i("buttonClick", "Relais 2 was clicked");
-            communicationInterface.sendText("relais2");
-        } else if (v.getId() == R.id.button_arduino1) {
-            Log.i("buttonClick", "Arduino 1 was clicked");
-            communicationInterface.sendText("arduino1");
-        } else if (v.getId() == R.id.button_arduino2) {
-            Log.i("buttonClick", "Arduino 2 was clicked");
-            communicationInterface.sendText("arduino2");
-        } else if (v.getId() == R.id.button_outlet1) {
-            Log.i("buttonClick", "Outlet 1 was clicked");
-            communicationInterface.sendText("outlet1");
-        } else if (v.getId() == R.id.button_outlet2) {
-            Log.i("buttonClick", "Outlet 2 was clicked");
-            communicationInterface.sendText("outlet2");
-        } else if (v.getId() == R.id.button_outlet3) {
-            Log.i("buttonClick", "Outlet 3 was clicked");
-            communicationInterface.sendText("outlet3");
-        } else if (v.getId() == R.id.button_songtitle) {
-            Log.i("buttonClick", "songtitle was clicked");
-            communicationInterface.sendText("songtitle");
-        } else if (v.getId() == R.id.button_bikeActivity) {
-            Log.i("buttonClick", "bike activity was clicked");
-            changeToBike();
-        } else if (v.getId() == R.id.button_settingsActivity) {
-            Log.i("buttonClick", "Setting activity was clicked");
-            changeToSettings();
-
-        } else if (v.getId() == R.id.button_plantoverviewActivity) {
-            Log.i("buttonClick", "PlantOverview activity was clicked");
-            changeToPlantOverview();
+    override fun onClick(v: View) {
+        if (v.id == R.id.button) {
+            Log.i("buttonClick", "Übernehmen was clicked")
+            communicationInterface!!.sendText("Stations;" + startText!!.text.toString() + ";" + destText!!.text.toString())
+        } else if (v.id == R.id.button_weather) {
+            Log.i("buttonClick", "weather button was clicked")
+            communicationInterface!!.sendText("weather")
+        } else if (v.id == R.id.time_button) {
+            Log.i("buttonClick", "time_button was clicked")
+            communicationInterface!!.sendText("changetime")
+        } else if (v.id == R.id.button_relais1) {
+            Log.i("buttonClick", "Relais 1 was clicked")
+            communicationInterface!!.sendText("relais1")
+        } else if (v.id == R.id.button_relais2) {
+            Log.i("buttonClick", "Relais 2 was clicked")
+            communicationInterface!!.sendText("relais2")
+        } else if (v.id == R.id.button_arduino1) {
+            Log.i("buttonClick", "Arduino 1 was clicked")
+            communicationInterface!!.sendText("arduino1")
+        } else if (v.id == R.id.button_arduino2) {
+            Log.i("buttonClick", "Arduino 2 was clicked")
+            communicationInterface!!.sendText("arduino2")
+        } else if (v.id == R.id.button_outlet1) {
+            Log.i("buttonClick", "Outlet 1 was clicked")
+            communicationInterface!!.sendText("outlet1")
+        } else if (v.id == R.id.button_outlet2) {
+            Log.i("buttonClick", "Outlet 2 was clicked")
+            communicationInterface!!.sendText("outlet2")
+        } else if (v.id == R.id.button_outlet3) {
+            Log.i("buttonClick", "Outlet 3 was clicked")
+            communicationInterface!!.sendText("outlet3")
+        } else if (v.id == R.id.button_songtitle) {
+            Log.i("buttonClick", "songtitle was clicked")
+            communicationInterface!!.sendText("songtitle")
+        } else if (v.id == R.id.button_bikeActivity) {
+            Log.i("buttonClick", "bike activity was clicked")
+            changeToBike()
+        } else if (v.id == R.id.button_settingsActivity) {
+            Log.i("buttonClick", "Setting activity was clicked")
+            changeToSettings()
+        } else if (v.id == R.id.button_plantoverviewActivity) {
+            Log.i("buttonClick", "PlantOverview activity was clicked")
+            changeToPlantOverview()
+        } else if (v.id == R.id.textViewSources) {
+            Log.i("buttonClick", "OpenSources activity was clicked")
+            openSourcesDialog()
         }
     }
 
-    private void changeToBike() {
-        Intent intent = new Intent(getApplicationContext(), BikeTourActivity.class);
-        startActivity(intent);
+    private fun openSourcesDialog() {
+        val builder = AlertDialog.Builder(this)
+        val titleView: TextView = TextView(this)
+        titleView.setPadding(36, 48, 36, 8)
+        titleView.setTextSize(16F)
+        titleView.setTextColor(Color.WHITE)
+        titleView.autoLinkMask
+        titleView.text =
+            "Pump icons created by Smashicons - Flaticon: https://www.flaticon.com/free-icons/pump\n\nPlant icons created by Freepik - Flaticon: https://www.flaticon.com/free-icons/plant\n\nOutlet icons created by DinosoftLabs - Flaticon: https://www.flaticon.com/free-icons/outlet\n\nWater drop icons created by DinosoftLabs - Flaticon: https://www.flaticon.com/free-icons/water-drop\""
+        builder.setCustomTitle(titleView)
+        builder.setCancelable(true)
+        builder.setPositiveButton("OK", null);
+        val dialog: Dialog = builder.create()
+        dialog.show()
     }
 
-    private void changeToSettings() {
-        Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
-        startActivity(intent);
+    private fun changeToBike() {
+        val intent = Intent(applicationContext, BikeTourActivity::class.java)
+        startActivity(intent)
     }
 
-    private void changeToPlantOverview() {
-        Intent intent = new Intent(getApplicationContext(), PlantOverview.class);
-        startActivity(intent);
+    private fun changeToSettings() {
+        val intent = Intent(applicationContext, SettingsActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun changeToPlantOverview() {
+        val intent = Intent(applicationContext, PlantOverview::class.java)
+        startActivity(intent)
     }
 }
